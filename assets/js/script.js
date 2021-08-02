428a1074a0d09b4ad1259708531d9aca3e7d4708
Shirly <shirlyd@axioned.com>
2021-08-02 23:10:12 +0530

    final js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,39 +3,42 @@
 */
 var start = 0, end = 6;
 var abc = document.querySelector('button');
-abc.addEventListener('click', function() {loadMore(start, end)});
+abc.addEventListener('click', function () { loadMore(start, end) });
 
 
 function loadMore(begin = 0, last = 6) {
     var http = new XMLHttpRequest();
-    http.onload = function () {
+    http.onload = function () 
+    {
+        console.log(last);
         var a = JSON.parse(this.responseText);
-        console.log(a);
-        for (var i = begin; i < last; i++) 
-        {
-            var title = document.createElement('h4');
-            title.textContent = "Title: " + a[i].title;
-            console.log(document.getElementById('result'));
-            document.getElementById('result').appendChild(title);
-           
-            var body = document.createElement('p');
-            body.textContent = "Body: " + a[i].body;
-            document.getElementById('result').appendChild(body);
-            
-            
+        for (var i = begin; i < last; i++) {
+            if (i <= a.length -1)
+            {
+                var title = document.createElement('h4');
+                title.textContent = "Title: " + a[i].title;
+                console.log(document.getElementById('result'));
+                document.getElementById('result').appendChild(title);
+
+                var body = document.createElement('p');
+                body.textContent = "Body: " + a[i].body;
+                document.getElementById('result').appendChild(body);
+            }
+
+            if (i == a.length - 1) 
+            {
+                document.querySelector('button').style.display = "none";
+            }
             console.log(a[i]);
-            
-            
-            // document.getElementById('result').innerHTML = this.responseText;
         }
-        
-       
+
+
     }
     http.open("GET", "https://jsonplaceholder.typicode.com/posts");
     http.send();
     start += 6;
     end += 6;
-    
+
 }
 loadMore(start, end);
 
